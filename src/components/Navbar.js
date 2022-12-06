754c6ad1ceb9046adacde394844981b25a7cdfc6
Divas Kumar Gupta <dvsmathur7050@gmail.com>
2022-12-07 04:46:19 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,10 +17,6 @@ export default function Navbar(props) {
             <a className="nav-link" href="/">{props.aboutText}</a>
           </li>
         </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
@@ -34,5 +30,5 @@ Navbar.propTypes={
 
 Navbar.defaultProps={
     title: 'set_title_here',
-    aboutText:'About_from_defualt'
+    aboutText:''
 }
